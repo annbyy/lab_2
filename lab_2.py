6ae53b4f4624f5ea21218b0eca5a2b32a117dfce
Ana Bondari <98388544+annbyy@users.noreply.github.com>
2023-10-15 23:17:50 +0300

    Add files via upload
    Сохранение данных в выходном файле .csv

--- lab_2.py
+++ lab_2.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 import pandas as pd
 
 headers = {'accept':'*/*', 'user-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36'}
@@ -13,6 +14,15 @@ table = soup.find('table')
 data = table.find_all('td')
 count = table.find_all('tr')
 
+data_header = ["Число", "Температура", "Давление", "Ветер"]
+data_rows = []
 
 for i in range(len(count)-2):
-    print("{} октября: день - {}°С, {}мм рт ст, ветер {}; вечер - {}°С, {}мм рт ст, ветер {};".format(data[i*11].text, data[i*11+1].text, data[i*11+2].text, data[i*11+5].text, data[i*11+6].text, data[i*11+7].text, data[i*11+10].text))
+    data_rows.append(["{} октября".format(data[i*11].text), "{}°С".format(data[i*11+1].text), "{}мм рт ст".format(data[i*11+2].text), "ветер {}".format(data[i*11+5].text)])
+
+filename = 'Weather_Data.csv'
+with open(filename, 'w', newline="") as file:
+    csvwriter = csv.writer(file)
+    csvwriter.writerow(data_header)
+    csvwriter.writerows(data_rows)
+file.close()
